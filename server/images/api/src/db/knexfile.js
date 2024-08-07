e414d3777234c7df72abc22f177d82a52058ce05
Cambur Dmitr <dmitr.cambur@student.ehb.be>
2024-08-07 17:39:01 +0200

    updated knexfile.js for pipeline

--- server/images/api/src/db/knexfile.js
+++ server/images/api/src/db/knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: "localhost",
+      host: "store",
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB || "postgres",
